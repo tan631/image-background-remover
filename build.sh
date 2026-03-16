aaf1dc8b6bc9d76717a4d8eeb21a58105dba0d0f
tan631 <tanwuye810@gmail.com>
2026-03-16 14:48:08 +0800

    fix: copy all open-next output into assets dir for Cloudflare Pages

--- build.sh
+++ build.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 set -e
 npx @opennextjs/cloudflare build
-# Cloudflare Pages needs _worker.js in the assets output dir
-if [ -f .open-next/worker.js ]; then
-  cp .open-next/worker.js .open-next/assets/_worker.js
-  echo "Copied worker.js to assets/_worker.js"
+
+# Copy all non-assets files into assets dir so _worker.js can resolve them
+cd .open-next
+for item in *; do
+  if [ "$item" != "assets" ]; then
+    cp -r "$item" "assets/$item"
+  fi
+done
+
+# Rename worker.js to _worker.js
+if [ -f assets/worker.js ]; then
+  mv assets/worker.js assets/_worker.js
+  echo "Renamed worker.js to _worker.js"
 fi
+
+echo "Build output ready in .open-next/assets"
