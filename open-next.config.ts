feca305e8397865af981a0d12a5a90ef4fb7c627
tan631 <tanwuye810@gmail.com>
2026-03-16 10:36:01 +0800

    fix: correct open-next.config.ts format

--- open-next.config.ts
+++ open-next.config.ts
@@ -5,17 +5,22 @@ const config: OpenNextConfig = {
     override: {
       wrapper: "cloudflare-node",
       converter: "edge",
-      incrementalCache: async () => (await import("@opennextjs/cloudflare")).KVIncrementalCache,
+      proxyExternalRequest: "fetch",
+      incrementalCache: "dummy",
       tagCache: "dummy",
       queue: "dummy",
     },
   },
+  edgeExternals: ["node:crypto"],
   middleware: {
     external: true,
     override: {
       wrapper: "cloudflare-edge",
       converter: "edge",
       proxyExternalRequest: "fetch",
+      incrementalCache: "dummy",
+      tagCache: "dummy",
+      queue: "dummy",
     },
   },
 };
